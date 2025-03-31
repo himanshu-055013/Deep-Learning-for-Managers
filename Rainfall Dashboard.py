c9d425bc0ceb5bba9cb6ac0a39607159efdbfa56
himanshu-055013 <055013@fsm.ac.in>
2025-04-01 01:08:41 +0530

    Rainfall Dashboard.py

--- Rainfall Dashboard.py
+++ Rainfall Dashboard.py
@@ -17,6 +17,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from datetime import datetime
 
+pip install plotly
+
+
 # Load the dataset
 @st.cache_data  # Cache data to improve performance
 def load_data(file_path):
